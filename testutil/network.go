e6092bdaf317a0f3a001f34ec03c5379a3a84a1b
PJ <peterjan.brone@gmail.com>
2024-04-17 19:54:13 +0200

    testutil: remove MineBlock in favour of impl in coreutils package

--- testutil/network.go
+++ testutil/network.go
@@ -1,11 +1,8 @@
 package testutil
 
 import (
-	"time"
-
 	"go.sia.tech/core/consensus"
 	"go.sia.tech/core/types"
-	"go.sia.tech/coreutils"
 	"go.sia.tech/coreutils/chain"
 )
 
@@ -23,26 +20,4 @@ func Network() (*consensus.Network, types.Block) {
 	n.HardforkV2.AllowHeight = 200 // comfortably above MaturityHeight
 	n.HardforkV2.RequireHeight = 250
 	return n, genesisBlock
-}
-
-// MineBlock mines a block with the given transactions, transaction fees are
-// added to the miner payout.
-func MineBlock(cm *chain.Manager, minerAddress types.Address) types.Block {
-	state := cm.TipState()
-	b := types.Block{
-		ParentID:     state.Index.ID,
-		Timestamp:    types.CurrentTimestamp(),
-		Transactions: cm.PoolTransactions(),
-		MinerPayouts: []types.SiacoinOutput{{Address: minerAddress, Value: state.BlockReward()}},
-	}
-
-	// add txn fees to miner payout
-	for _, txn := range b.Transactions {
-		b.MinerPayouts[0].Value = b.MinerPayouts[0].Value.Add(txn.TotalFees())
-	}
-
-	if !coreutils.FindBlockNonce(state, &b, 5*time.Second) {
-		panic("failed to find nonce")
-	}
-	return b
 }
