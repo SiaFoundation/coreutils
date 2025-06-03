2e3d053652642dab9e5c8d744570ca89f4fa8bf1
PJ <peterjan.brone@gmail.com>
2025-06-03 09:50:28 +0200

    wallet: remove tip from memory

--- testutil/wallet.go
+++ testutil/wallet.go
@@ -129,15 +129,16 @@ func (es *EphemeralWalletStore) WalletEventCount() (uint64, error) {
 	return uint64(len(es.events)), nil
 }
 
-// UnspentSiacoinElements returns the wallet's unspent siacoin outputs.
-func (es *EphemeralWalletStore) UnspentSiacoinElements() (utxos []types.SiacoinElement, _ error) {
+// UnspentSiacoinElements returns the wallet's unspent siacoin outputs as well
+// as the last indexed tip of the wallet.
+func (es *EphemeralWalletStore) UnspentSiacoinElements() (utxos []types.SiacoinElement, tip types.ChainIndex, _ error) {
 	es.mu.Lock()
 	defer es.mu.Unlock()
 
 	for _, se := range es.utxos {
 		utxos = append(utxos, se.Copy())
 	}
-	return utxos, nil
+	return utxos, es.tip, nil
 }
 
 // Tip returns the last indexed tip of the wallet.
