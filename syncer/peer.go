e6158be158774ba8227ed37d22f6e0900bb04839
Nate <me@n8m.us>
2025-06-20 07:13:11 -0700

    remove commitment check

--- syncer/peer.go
+++ syncer/peer.go
@@ -174,10 +174,12 @@ func (p *Peer) SendCheckpoint(index types.ChainIndex, timeout time.Duration) (ty
 			err = errors.New("checkpoint is not a v2 block")
 		} else if r.Block.ID() != index.ID {
 			err = errors.New("checkpoint has wrong index")
-		} else if r.Block.V2.Height > r.State.Network.HardforkV2.RequireHeight &&
+		}
+		// note: Network is nil because we don't encode it
+		/*else if r.Block.V2.Height > r.State.Network.HardforkV2.RequireHeight &&
 			r.Block.V2.Commitment != r.State.Commitment(r.Block.MinerPayouts[0].Address, r.Block.Transactions, r.Block.V2Transactions()) {
 			err = errors.New("checkpoint has wrong commitment")
-		}
+		}*/
 	}
 	return r.Block, r.State, err
 }
