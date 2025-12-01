fb4ddc9612301fbf199a8d679b42ac2788a0dc9d
Chris Schinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2025-12-01 17:31:23 +0100

    100k

--- rhp/v4/quic/quic.go
+++ rhp/v4/quic/quic.go
@@ -29,7 +29,7 @@ const (
 
 	// maxIncomingStreams is the maximum number of incoming streams allowed per
 	// QUIC connection by the server.
-	maxIncomingStreams = 1000
+	maxIncomingStreams = 100000
 )
 
 type (
@@ -68,7 +68,7 @@ func WithTLSConfig(fn func(*tls.Config)) ClientOption {
 func (s *stream) Close() error {
 	err := s.Stream.Close()
 	_, errCopy := io.CopyN(io.Discard, s, 4096)
-	if !errors.Is(errCopy, io.EOF) && !errors.Is(errCopy, io.ErrUnexpectedEOF) {
+	if !errors.Is(errCopy, io.EOF) {
 		// fall back to forcefully canceling the read if we couldn't reach EOF
 		s.CancelRead(1)
 	}
